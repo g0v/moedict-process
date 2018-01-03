aec180e7859a12eee561d36c1c6db054e8e49708
Chad Liu <chadliu23@happytv.com.tw>
2018-01-03 13:08:18 +0800

    Fix #24 references to Unicode PNG to Unicode chracters

--- parse.py
+++ parse.py
@@ -186,6 +186,7 @@ def normalize(s):
     if not (isinstance(s, unicode) or isinstance(s, str)):
         return ""
     s = re.sub(r'&(\w*?)\._104_0\.gif;?', r'{[\1]}', s)
+    s = re.sub(r'&([0-9a-fA-F]*?);?_\.png;?', r'{[\1]}', s)
     return s
 
 
